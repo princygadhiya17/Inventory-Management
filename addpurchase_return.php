d201e9b5420cbe34b444d0be30c1d2cafbcfa6f1
princy <23030401042@darshan.ac.in>
2026-05-27 12:14:42 +0530

    purchasereturn

--- addpurchase_return.php
+++ addpurchase_return.php
@@ -1,12 +1,12 @@
-    <?php
-    include("config.php");
-    include("check_session.php");
-    ?>
+<?php
+include("config.php");
+include("check_session.php");
+?>
 
-    <!DOCTYPE html>
+<!DOCTYPE html>
 
-    <html>
-    <?php include("header.php"); ?>
+<html>
+<?php include("header.php"); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -166,7 +166,7 @@
                                                     <div class="input-group">
                                                         <span class="input-group-text">₹</span>
                                                         <input type="number" step="0.01" class="form-control" name="paid_amount" id="paid_amount" placeholder="Enter Paid Amount" value="<?php if (isset($_GET['id'])) {
-                                                                                                                                                                                                echo $fetch1['paid_amount'];
+                                                                                                                                                                                                echo $fetch1['paid_amount'] ?? '';
                                                                                                                                                                                             } ?>" required>
                                                     </div>
                                                 </div>
