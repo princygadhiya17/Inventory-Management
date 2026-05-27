fab096354933ea0037ee16c74cad354336945754
princy <23030401042@darshan.ac.in>
2026-05-27 11:42:46 +0530

    salesreturn final

--- addsales_return.php
+++ addsales_return.php
@@ -3,9 +3,8 @@ include("config.php");
 include("check_session.php");
 ?>
 <!DOCTYPE html>
-
-    <html>
-    <?php include("header.php"); ?>
+<html>
+<?php include("header.php"); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
