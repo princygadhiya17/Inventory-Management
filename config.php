48e5662a28d3e728292628d54e4bc8bc69ef11a8
princy <23030401042@darshan.ac.in>
2026-05-27 11:16:31 +0530

    config commit final

--- config.php
+++ config.php
@@ -1,13 +1,3 @@
-// $conn = mysqli_connect('127.0.0.1', 'root', '', 'inventory_management');
-// if($conn)
-// {
-//     echo " Connection Successfull";
-// }
-// else{
-//     echo " Connection fail";
-
-// }
-
 <?php
 $conn = mysqli_connect(
     'zephyr.proxy.rlwy.net',
