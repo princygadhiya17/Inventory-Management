bac797effed178601160a56aa53d9ca3eaf5222d
princy <23030401042@darshan.ac.in>
2026-05-28 10:41:49 +0530

    login

--- login.php
+++ login.php
@@ -466,27 +466,6 @@
       </form>
 
       <div class="or-row">or continue with</div>
-
-      <div class="social-row">
-        <button class="social-btn" type="button" onclick="googleLogin()">
-          <svg width="15" height="15" viewBox="0 0 24 24">
-            <path fill="#EA4335" d="M5.266 9.765A7.077 7.077 0 0 1 12 4.909c1.69 0 3.218.6 4.418 1.582L19.91 3C17.782 1.145 15.055 0 12 0 7.27 0 3.198 2.698 1.24 6.65l4.026 3.115Z" />
-            <path fill="#34A853" d="M16.04 18.013c-1.09.703-2.474 1.078-4.04 1.078a7.077 7.077 0 0 1-6.723-4.823l-4.04 3.067A11.965 11.965 0 0 0 12 24c2.933 0 5.735-1.043 7.834-3l-3.793-2.987Z" />
-            <path fill="#4A90E2" d="M19.834 21c2.195-2.048 3.62-5.096 3.62-9 0-.71-.109-1.473-.272-2.182H12v4.637h6.436c-.317 1.559-1.17 2.766-2.395 3.558L19.834 21Z" />
-            <path fill="#FBBC05" d="M5.277 14.268A7.12 7.12 0 0 1 4.909 12c0-.782.125-1.533.357-2.235L1.24 6.65A11.934 11.934 0 0 0 0 12c0 1.92.445 3.73 1.237 5.335l4.04-3.067Z" />
-          </svg>
-
-          Google
-        </button>
-        <button class="social-btn" type="button">
-          <svg width="15" height="15" viewBox="0 0 24 24">
-            <path fill="#1877F2" d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.235 2.686.235v2.97h-1.513c-1.491 0-1.956.93-1.956 1.886v2.253h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073Z" />
-          </svg>
-          Facebook
-        </button>
-      </div>
-
-      <div class="signup-row">Not a member? <a href="#">Signup now</a></div>
     </div>
 
   </div>
@@ -501,7 +480,7 @@
     });
   </script>
 </body>
-<script type="module">
+<!-- <script type="module">
   import {
     initializeApp
   }
@@ -578,6 +557,6 @@
       window.location = "dashboard.php";
 
     });
-</script>
+</script> -->
 
 </html>
